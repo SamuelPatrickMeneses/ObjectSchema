6780dfeb46ef09d79609d6fc95a12e237879b779
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2022-12-03 12:46:03 -0300

    importation bug correction

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const arraySchema = require("./src/ES/arraySchema.js");
 const booleanSchema = require("./src/ES/booleanSchema.js");
 const numberSchema = require("./src/ES/numberSchema.js");
 const stringSchema = require("./src/ES/stringSchema.js");
-const objectSchema = request('./src/ES/objectSchema.js');
+const objectSchema = require('./src/ES/objectSchema.js');
 module.exports = {arraySchema,booleanSchema,numberSchema,stringSchema,objectSchema};
